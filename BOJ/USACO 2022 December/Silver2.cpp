ed4cb5f775064abe6943fd6bb192230b773d3603
Sam <notek0000@gmail.com>
2023-05-10 14:12:20 +0900

    Fun Problem +1

--- BOJ/USACO 2022 December/Silver2.cpp
+++ BOJ/USACO 2022 December/Silver2.cpp
@@ -2,7 +2,49 @@
 
 using namespace std;
 #define int long long
+#define MAX 5000000LL
+
+int N, A[200001], john[200001];
+bool habsung[MAX + 1];
+
+void erathos() {
+    for (int i = 2; i <= MAX; i++) {
+        if (!habsung[i]) {
+            for (int j = i * 2; j <= MAX; j += i) {
+                habsung[j] = true;
+            }
+        }
+    }
+}
+
+void solve() {
+    
+    cin >> N;
+    for (int i = 1; i <= N; i++) {
+        cin >> A[i];
+    }
+
+    int mini = 987654321234567;
+    for (int i = 1; i <= N; i++) {
+        // if Even:
+        if (A[i] % 2 == 0) {
+            john[i] = A[i] / 2;
+        }
+        else {
+            if (habsung[A[i]]) john[i] = 3;
+            else john[i] = 1;
+        }
+        if (mini / 2 > john[i] / 2) mini = john[i];
+    }
+
+    if (mini % 2 == 0) cout << "Farmer Nhoj" << '\n';
+    else cout << "Farmer John" << '\n';
+    
+}
 
 signed main() {
-    int x;
+    erathos();
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+    int T; cin >> T; while (T--) solve();
 }
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,54 +1,28 @@
-#include <iostream>
-#include <vector>
-
-using namespace std;
-
-const int base = 1000000000; // 10^9
-const int base_digits = 9;
-
-vector<int> to_vector(string str) {
-    vector<int> res;
-    for (int i = str.size(); i > 0; i -= base_digits) {
-        int x = 0;
-        for (int j = max(0, i - base_digits); j < i; j++)
-            x = x * 10 + (str[j] - '0');
-        res.push_back(x);
-    }
-    return res;
-}
-
-void print(vector<int> v) {
-    cout << v.back();
-    for (int i = v.size() - 2; i >= 0; i--) {
-        cout.width(base_digits);
-        cout.fill('0');
-        cout << v[i];
-    }
-}
-
-vector<int> operator*(const vector<int>& a, const vector<int>& b) {
-    vector<int> res(a.size() + b.size());
-    for (int i = 0; i < a.size(); i++)
-        for (int j = 0, carry = 0; j < b.size() || carry; j++) {
-            long long cur = res[i+j] + (long long) a[i] * (j < b.size() ? b[j] : 0) + carry;
-            res[i+j] = cur % base;
-            carry = cur / base;
-        }
-    while (res.size() > 1 && res.back() == 0)
-        res.pop_back();
-    return res;
-}
-
-vector<int> power(const vector<int>& a, int b) {
-    if (b == 0) return vector<int>(1,1);
-    vector<int> res = power(a, b/2);
-    res = res * res;
-    if (b % 2 == 1) res = res * a;
-    return res;
-}
-
-int main() {
-    vector<int> res = power(to_vector("2"), 100000);
-    print(res);
-    return 0;
-}
+/*
+1 -> J (1)
+2 -> J (1)
+3 -> J (1)
+4 -> N (2)
+5 -> J (1)
+6 -> J (3)
+7 -> J (1)
+8 -> N (4)
+9 -> J (3)
+10 -> J (5)
+11 -> J (1)
+12 -> N (6)
+13 -> J (1)
+14 -> J (7)
+15 -> J (3)
+16 -> N (8)
+17 -> J (1)
+18 -> J (9)
+19 -> J (1)
+20 -> N (10)
+21 -> J (3)
+...
+if odd: John wins.
+if even:
+    if can be divided by 4: Nhoj wins.
+    else: John wins.
+*/
