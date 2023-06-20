78f864d676e1a64a68d6668210ae74e902cb75fc
Sam Willington <last_eyes@protonmail.com>
2023-06-20 11:36:48 +0900

    a

--- dreamhack/public.cpp
+++ dreamhack/public.cpp
@@ -11,11 +11,18 @@ int main() {
     QWORD n = p * q;
     QWORD d = 1384538333;
 
-    QWORD first = 2580409063;
-    QWORD v4 = 1;
-    for (QWORD i = 0; i < d; i++) {
-        v4 = first * v4 % n;
+    QWORD first[] = {3571245376,2709774306,3278103302,4191347304,3290047497,714850555,2588811957,2100883354,2975830687,2664856559, 2580409063};
+    
+    for (int k = 0; k < 11; k++) {
+        QWORD v4 = 1;
+        for (QWORD i = 0; i < d; i++) {
+            v4 = first[k] * v4 % n;
+        }
+        while (v4) {
+            unsigned char s = v4 % 256;
+            cout << s;
+            v4 /= 256;
+        }
     }
-    cout << hex << v4 << '\n';
-    //DH{_RSA_1s_public-pr1vate-key_crypt0gr@phy!}
+    //DH{_RSA_1s_pub1ic-pr1v4te-key_crypt0gr@phy!}
 }
